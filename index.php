b108fc419e7079e67667007721c7182475183fed
Your Name <imbotgames30@gmail.com>
2026-05-08 20:26:06 -0600

    PruebaInicial

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <title>Empleados</title>
 </head>
 <body>
-    
-    
-    
+
+  <h1>Lista de empleados</h1>
+  <pre id="output"></pre>
+
+  <script>
+    fetch("http://localhost/AsisProyecto/AsisBackend/api/empleados.php")
+      .then(res => res.json())
+      .then(data => {
+        console.log("Empleados:", data);
+
+        document.getElementById("output").textContent =
+          JSON.stringify(data, null, 2);
+      })
+      .catch(err => console.error("Error:", err));
+  </script>
+
 </body>
 </html>

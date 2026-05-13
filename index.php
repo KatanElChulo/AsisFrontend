ed4ac1196e95e33fe379cf8bbc79d11b01518838
MOIIESK <moyesca73@gmail.com>
2026-05-13 11:29:18 -0600

    se integro en el index el login de manera funcional, se modifico index, y se creo otra page de asistencia

--- index.php
+++ index.php
@@ -2,23 +2,70 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Empleados</title>
+  <title>Login - Sistema de Asistencia</title>
 </head>
 <body>
 
-  <h1>Lista de empleados</h1>
-  <pre id="output"></pre>
+  <h1>Iniciar sesión</h1>
+
+  <form id="loginForm">
+    <label>Correo:</label>
+    <input type="email" id="correo" required>
+
+    <br><br>
+
+    <label>Contraseña:</label>
+    <input type="password" id="password" required>
+
+    <br><br>
+
+    <button type="submit">Entrar</button>
+  </form>
+
+  <p id="mensaje"></p>
 
   <script>
-    fetch("http://localhost/AsisProyecto/AsisBackend/api/empleados.php")
+    const form = document.getElementById("loginForm");
+    const mensaje = document.getElementById("mensaje");
+
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+
+      const correo = document.getElementById("correo").value;
+      const password = document.getElementById("password").value;
+
+      fetch("http://localhost/AsisProyecto/AsisBackend/api/login.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          correo: correo,
+          password: password
+        })
+      })
       .then(res => res.json())
       .then(data => {
-        console.log("Empleados:", data);
+        console.log(data);
 
-        document.getElementById("output").textContent =
-          JSON.stringify(data, null, 2);
+        if (data.success) {
+          localStorage.setItem("usuario", JSON.stringify(data.usuario));
+
+          if (data.rol === "ADMIN") {
+            window.location.href = "pages/empleados.php";
+          } else {
+            window.location.href = "pages/asistencia.php";
+          }
+
+        } else {
+          mensaje.textContent = data.message;
+        }
       })
-      .catch(err => console.error("Error:", err));
+      .catch(error => {
+        console.error("Error:", error);
+        mensaje.textContent = "Error al conectar con el servidor";
+      });
+    });
   </script>
 
 </body>
---
+++ pages/Asistencia.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Asistencia</title>
+</head>
+<body>
+
+  <h1>Registro de asistencia</h1>
+  <p>Bienvenido esbirro</p>
+
+</body>
+</html>

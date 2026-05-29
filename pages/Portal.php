de42f667003363fa0798b5eb61de01f5220729b0
Your Name <imbotgames30@gmail.com>
2026-05-28 19:14:04 -0600

    afdsaf

--- pages/Portal.php
+++ pages/Portal.php
@@ -11,7 +11,7 @@
 
     <link rel="stylesheet" href="../css/style.css">
 
-    <link rel="stylesheet" href="../css/portal.css">
+    <link rel="stylesheet" href="../css/portal.css?v=20">
 
 </head>
 

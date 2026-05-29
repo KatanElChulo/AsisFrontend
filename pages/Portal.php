9ce2feb5d10ba1badcac0401454f610f76eac9f5
Your Name <imbotgames30@gmail.com>
2026-05-28 19:08:15 -0600

    erfs

--- pages/Portal.php
+++ pages/Portal.php
@@ -11,7 +11,7 @@
 
     <link rel="stylesheet" href="../css/style.css">
 
-    <link rel="stylesheet" href="../css/portal.css?v=5">
+    <link rel="stylesheet" href="../css/portal.css?v=10">
 
 </head>
 
